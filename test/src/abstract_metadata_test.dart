9e6dfb12e7e3bba6ec60cc80d2596e8126403cb0
Don <don.j.olmstead@gmail.com>
2016-10-24 23:44:34 -0700

    Add abstract metadata tests

---
+++ test/src/abstract_metadata_test.dart
@@ -0,0 +1,46 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_analyzer/metadata.dart';
+import 'package:dogma_source_metadata_test/test.dart';
+import 'package:test/test.dart';
+
+import 'matcher.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+final Metadata _abstractMetadata = new ClassMetadata('A', isAbstract: true);
+final Metadata _concreteMetadata = new ClassMetadata('C', isAbstract: false);
+final Metadata _notAbstractMetadata = new ParameterMetadata('param');
+
+void main() {
+  test('isAbstractMetadata', () {
+    final matcher = isAbstractMetadata;
+
+    expectMatch(matcher, _abstractMetadata);
+    expectMatch(matcher, _concreteMetadata);
+    expectNoMatch(matcher, _notAbstractMetadata);
+  });
+  test('isAbstract', () {
+    final matcher = isAbstract;
+
+    expectMatch(matcher, _abstractMetadata);
+    expectNoMatch(matcher, _concreteMetadata);
+    expectNoMatch(matcher, _notAbstractMetadata);
+  });
+  test('isConcrete', () {
+    final matcher = isConcrete;
+
+    expectNoMatch(matcher, _abstractMetadata);
+    expectMatch(matcher, _concreteMetadata);
+    expectMatch(matcher, _notAbstractMetadata);
+  });
+}
