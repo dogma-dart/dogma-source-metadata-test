c7dc2cbc8ba8d8eb8fa8117c9c759129ce43c70a
Don <don.j.olmstead@gmail.com>
2016-10-25 00:37:31 -0700

    Add constructor metadata matcher

---
+++ lib/src/constructor_metadata_matcher.dart
@@ -0,0 +1,44 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_analyzer/metadata.dart';
+
+import 'function_metadata_matcher.dart';
+import 'metadata_matcher.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// A [MetadataMatcher] for [ConstructorMetadata] equality.
+class ConstructorMetadataMatcher extends FunctionMetadataMatcher<ConstructorMetadata> {
+  //---------------------------------------------------------------------
+  // Construction
+  //---------------------------------------------------------------------
+
+  /// Creates an instance of [ConstructorMetadataMatcher] which compares metadata
+  /// using the values in [expected].
+  ConstructorMetadataMatcher(ConstructorMetadata expected)
+      : super(expected);
+
+  //---------------------------------------------------------------------
+  // Matcher
+  //---------------------------------------------------------------------
+
+  @override
+  bool matches(dynamic item, Map matchState) {
+    var matched = super.matches(item, matchState);
+    final actual = item as ConstructorMetadata;
+
+    matched = checkField(matchState, 'isConst', actual.isConst, expected.isConst) && matched;
+    matched = checkField(matchState, 'isFactory', actual.isFactory, expected.isFactory) && matched;
+
+    return matched;
+  }
+}
