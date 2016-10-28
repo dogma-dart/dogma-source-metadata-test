584cce2b3c477db0da33c1658f23882ee9c4d65c
Don <don.j.olmstead@gmail.com>
2016-10-27 18:32:41 -0700

    Rework annotation checks

--- lib/src/annotated.dart
+++ lib/src/annotated.dart
@@ -14,27 +14,15 @@ import 'package:test/test.dart';
 // Library contents
 //---------------------------------------------------------------------
 
-/// Determine if the [expected] annotation is present on metadata.
-///
-/// This assumes that [expected] can be compared. If not then it will not
-/// function properly. A [description] can be added if necessary.
-Matcher annotatedWith(dynamic expected, [String description]) =>
-    predicate((value) {
-      if (value is! Annotated) return false;
+/// Function for checking if the [actual] and [expected] values are equal.
+typedef bool AnnotationEqual<T>(dynamic actual, T expected);
 
-      return (value as Annotated).annotations.contains(expected);
-    }, description ?? 'has annotation $expected');
+/// Determine if the metadata has any annotations.
+Matcher isAnnotated = predicate((value) {
+  if (value is! Annotated) return false;
 
-/// Determine if the [expected] annotation is not present on metadata.
-///
-/// This assumes that [expected] can be compared. If not then it will not
-/// function properly. A [description] can be added if necessary.
-Matcher notAnnotatedWith(dynamic expected, [String description]) =>
-    predicate((value) {
-      if (value is! Annotated) return true;
-
-      return (value as Annotated).annotations.contains(expected);
-    }, description ?? 'has annotation $expected');
+  return (value as Annotated).annotations.isNotEmpty;
+});
 
 /// Determine if the metadata does not contain any annotations.
 Matcher notAnnotated = predicate((value) {
@@ -42,3 +30,85 @@ Matcher notAnnotated = predicate((value) {
 
   return (value as Annotated).annotations.isEmpty;
 });
+
+/// Determines if the metadata annotations is the expected [length].
+Matcher hasAnnotations(int length) =>
+    predicate((value) {
+      if (value is! Annotated) return false;
+
+      return (value as Annotated).annotations.length == length;
+    });
+
+/// Uses the [equal] function to determine if an annotation is present on the
+/// metadata.
+///
+/// A [description] can added if necessary which will give information on what
+/// is being checked for.
+Matcher hasAnnotation/*<T>*/(AnnotationEqual/*<T>*/ equal,
+                             dynamic/*=T*/ expected,
+                            [String description]) =>
+    predicate(
+        (value) => _hasAnnotation/*<T>*/(value, expected, equal),
+        description ?? 'has annotation'
+    );
+
+/// Uses the [equal] function to determine if an annotation is not present on
+/// the metadata.
+///
+/// A [description] can added if necessary which will give information on what
+/// is being checked for.
+Matcher missingAnnotation/*<T>*/(AnnotationEqual/*<T>*/ equal,
+                                 dynamic/*=T*/ expected,
+                                [String description]) =>
+    predicate(
+        (value) => !_hasAnnotation/*<T>*/(value, expected, equal),
+        description ?? 'missing annotation'
+    );
+
+/// Determine if the [expected] annotation is present on metadata.
+///
+/// This function can be used directly when an annotation contains an equality
+/// operator or when there is a single instance of an annotation such as
+/// `override`. Otherwise use [hasAnnotation] and provide a comparison
+/// function.
+///
+/// A [description] can added if necessary which will give information on what
+/// is being checked for.
+Matcher annotatedWith/*<T>*/(dynamic/*=T*/ expected, [String description]) =>
+    hasAnnotation/*<T>*/(
+        _isEqual,
+        expected,
+        description ?? 'has annotation $expected'
+    );
+
+/// Determine if the [expected] annotation is not present on metadata.
+///
+/// This function can be used directly when an annotation contains an equality
+/// operator or when there is a single instance of an annotation such as
+/// `override`. Otherwise use [missingAnnotation] and provide a comparison
+/// function.
+///
+/// A [description] can added if necessary which will give information on what
+/// is being checked for.
+Matcher notAnnotatedWith/*<T>*/(dynamic/*=T*/ expected, [String description]) =>
+    missingAnnotation/*<T>*/(
+        _isEqual,
+        expected,
+        description ?? 'missing annotation $expected'
+    );
+
+/// Simple equality test of [actual] and [expected].
+bool _isEqual/*<T>*/(dynamic/*=T*/ actual, dynamic/*=T*/ expected) =>
+    actual == expected;
+
+/// Determines if the [annotated] metadata contain the [expected] value which
+/// is verified through the [equal] function.
+bool _hasAnnotation/*<T>*/(dynamic annotated,
+                           dynamic expected,
+                           AnnotationEqual/*<T>*/ equal) {
+  if (annotated is! Annotated) return false;
+
+  bool check(dynamic value) => equal(value, expected);
+
+  return annotated.annotations.firstWhere(check, orElse: () => null) != null;
+}
