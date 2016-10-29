e64cded5b1292754c7a569653cfe2815e7462e78
Don <don.j.olmstead@gmail.com>
2016-10-29 08:04:47 -0700

    Add matchers for functions

---
+++ lib/src/function_metadata.dart
@@ -0,0 +1,48 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_analyzer/metadata.dart';
+import 'package:test/test.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// A matcher for [FunctionMetadata] instances.
+const Matcher isFunctionMetadata = const isInstanceOf<FunctionMetadata>();
+
+/// A matcher for an empty parameter list.
+final Matcher noParameters = predicate((value) {
+  if (value is! FunctionMetadata) return true;
+
+  return (value as FunctionMetadata).parameters.isEmpty;
+}, 'function has no parameters');
+
+/// A matcher for a function which contains parameters.
+final Matcher hasParameters = predicate((value) {
+  if (value is! FunctionMetadata) return false;
+
+  return (value as FunctionMetadata).parameters.isNotEmpty;
+}, 'function has parameters');
+
+/// Matches the [length] of parameters on [FunctionMetadata].
+Matcher hasParameterLength(int length) =>
+    predicate((value) {
+      if (value is! FunctionMetadata) return false;
+
+      return (value as FunctionMetadata).parameters.length == length;
+    }, 'function has $length parameters');
+
+/// Matches the return [type] of [FunctionMetadata].
+Matcher returnsType(TypeMetadata type) =>
+    predicate((value) {
+      if (value is! FunctionMetadata) return false;
+
+      return (value as FunctionMetadata).returnType == type;
+    });
