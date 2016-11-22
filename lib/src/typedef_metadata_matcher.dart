7444f3af1b4be3d0bc8db077d3236b6844111b77
Don <don.j.olmstead@gmail.com>
2016-11-21 23:17:30 -0800

    Add typedef metadata matcher

---
+++ lib/src/typedef_metadata_matcher.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_analyzer/metadata.dart';
+
+import 'function_metadata_matcher.dart';
+import 'metadata_matcher.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// A [MetadataMatcher] for [MethodMetadata] equality.
+class TypedefMetadataMatcher extends FunctionMetadataMatcher<TypedefMetadata> {
+  //---------------------------------------------------------------------
+  // Construction
+  //---------------------------------------------------------------------
+
+  /// Creates an instance of [TypedefMetadataMatcher] which compares metadata
+  /// using the values in [expected].
+  TypedefMetadataMatcher(TypedefMetadata expected)
+      : super(expected);
+}
