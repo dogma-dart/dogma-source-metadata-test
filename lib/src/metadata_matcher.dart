26fd7189a932b4b87c00f52d5f52fa5183b22747
Don <don.j.olmstead@gmail.com>
2016-11-21 23:14:13 -0800

    Add checks for lists of metadata

--- lib/src/metadata_matcher.dart
+++ lib/src/metadata_matcher.dart
@@ -11,6 +11,8 @@ import 'package:dogma_source_analyzer/metadata.dart';
 import 'package:meta/meta.dart';
 import 'package:test/test.dart';
 
+import 'metadata.dart';
+
 //---------------------------------------------------------------------
 // Library contents
 //---------------------------------------------------------------------
@@ -71,11 +73,65 @@ class MetadataMatcher<T extends Metadata> extends Matcher {
   /// added to the [matchState] under the [name].
   @protected
   bool checkField(Map matchState, String name, dynamic actual, dynamic expected) {
-    if (actual != expected) {
-      matchState[name] = 'expected ${expected.toString()} but actual is ${actual.toString()}';
-      return false;
+    final matcher = _matcher(expected);
+
+    return _checkMatch(matchState, name, actual, matcher);
+  }
+
+  /// Compares the values of [actual] vs [expected] respecting ordering.
+  ///
+  /// This should be used when the order of the values matters. If it does not
+  /// use [checkUnorderedList] instead.
+  @protected
+  bool checkOrderedList(Map matchState, String name, List actual, List expected) {
+    final matcher = equals(_matcherList(expected).toList());
+
+    return _checkMatch(matchState, name, actual, matcher);
+  }
+
+  /// Compares the values of [actual] vs [expected] without respecting
+  /// ordering.
+  ///
+  /// This should be used when the order of the values does not matter. If it
+  /// does use [checkOrderedList] instead.
+  @protected
+  bool checkUnorderedList(Map matchState, String name, List actual, List expected) {
+    final matcher = equals(_matcherList(expected).toSet());
+
+    return _checkMatch(matchState, name, actual, matcher);
+  }
+
+  //---------------------------------------------------------------------
+  // Private class methods
+  //---------------------------------------------------------------------
+
+  /// Checks the [actual] value against the [matcher].
+  static bool _checkMatch(Map matchState, String name, dynamic actual, Matcher matcher) {
+    final valueMatchState = {};
+    final matches = matcher.matches(actual, valueMatchState);
+
+    if (!matches) {
+      matchState[name] = valueMatchState;
     }
 
-    return true;
+    return matches;
+  }
+
+  /// Create matchers for the given list of [expected] values.
+  static Iterable<Matcher> _matcherList(List expected) =>
+      expected.map/*<Matcher>*/((value) => _matcher(value));
+
+  /// Creates a matcher for the [expected] value.
+  ///
+  /// If the [expected] value is metadata then a [MetadataMatcher] will be
+  /// returned. Otherwise it will do an [equals] check.
+  static Matcher _matcher(dynamic expected) {
+    var matcher;
+
+    if (expected is Metadata) {
+      matcher = metadataEqual(expected);
+    }
+
+    return matcher ?? equals(expected);
   }
 }
